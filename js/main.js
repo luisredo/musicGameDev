951a4c95d05309f5a620de6db3aafa1b3c8d9c72
salogelTorres <lalberto.torresd@gmail.com>
2020-09-19 19:36:10 +0200

    limpiamos main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 import { Listeners } from "./Infrastructure/Listeners.js";
-import { LoadContent } from "./Infrastructure/LoadContent.js";
 
 async function initAll() {
-  var menu = await new LoadContent();
-
   new Listeners();
 }
 initAll();
