d7cdb5e395dcb2726eea7c0b068828ba45bd889e
salogelTorres <lalberto.torresd@gmail.com>
2020-09-19 17:50:54 +0200

    save

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-import {LoadMenu} from "./Infrastructure/getData.js";
+import { LoadMenu } from "./Infrastructure/getData.js";
 import { Listeners } from "./Infrastructure/Listeners.js";
-import {  devShow} from "./Infrastructure/menu.js";
-
+import { devShow } from "./Infrastructure/menu.js";
 
 async function initAll() {
   var menu = await new LoadMenu();
-  
+
   await devShow(menu);
 
   new Listeners();
 }
 initAll();
-
